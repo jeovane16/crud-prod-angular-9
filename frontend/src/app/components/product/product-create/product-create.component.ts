ff7f17ef6f02c86fd22e215384d4f358705b47f0
jeovane <jeovanearaujo16@gmail.com>
2020-05-06 19:01:51 -0300

    chamada para o backend e adicão de novos produtos

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import {ProductService} from "../product.service";
+import {Product} from "../product.model";
 
 @Component({
   selector: 'app-product-create',
@@ -9,16 +10,25 @@ import {ProductService} from "../product.service";
 })
 export class ProductCreateComponent implements OnInit {
 
+  product: Product = {
+    name: '',
+    price: null
+  }
+
   constructor(private router: Router, private productService: ProductService) { }
 
   ngOnInit(): void {
   }
 
   createProduct(): void{
-    this.productService.showMessage("Produto criado com sucesso")
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage("Produto criado com sucesso");
+      this.router.navigate(['/products']);
+    });
+
   }
 
   cancel(): void {
-    this.router.navigate(['/products'])
+    this.router.navigate(['/products']);
   }
 }
--- frontend/src/app/components/product/product.model.ts
+++ frontend/src/app/components/product/product.model.ts
@@ -1,5 +1,5 @@
 export interface Product {
     id?: number,
     name: string,
-    prace: number
+    price: number
 }
